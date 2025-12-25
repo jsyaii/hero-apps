d76b4557590e6d9dd7c3205fe59ff71a0ada9cd1
jsyaii <jasiahasan1703@gmail.com>
2025-12-25 12:29:33 +0600

    third commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,21 +3,21 @@ import { RiLinkedinFill, RiTwitterXFill } from "react-icons/ri";
 import { MdFacebook } from "react-icons/md";
 import logo from "../../assets/logo.png";
 
-// const Footer = () => {
-//   const Links = [
-//     { to: "/", label: "Home" },
-//     { to: "/apps", label: "Apps" },
-//     { to: "/installation", label: "Installation" },
-//   ];
-//   return (
-//     <div>
-//       <footer className="bg-[#001931] text-gray-300 pt-[35px] pb-[30px] px-6">
-//         <div className="py-4 px-5 max-w-[1420px] mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
-//           <div>
-//             <NavLink
-//               to="/"
-//               className="font-bold text-2xl text-white flex items-center gap-1"
-//             >
+const Footer = () => {
+  const Links = [
+    { to: "/", label: "Home" },
+    { to: "/apps", label: "Apps" },
+    { to: "/installation", label: "Installation" },
+  ];
+  return (
+    <div>
+      <footer className="bg-[#001931] text-gray-300 pt-[35px] pb-[30px] px-6">
+        <div className="py-4 px-5 max-w-[1420px] mx-auto grid grid-cols-1 md:grid-cols-4 gap-6">
+          <div>
+            <NavLink
+              to="/"
+              className="font-bold text-2xl text-white flex items-center gap-1"
+            >
 //               <img className="w-[40px] h-[40px]" src={logo} alt="HERO.IO" />
 //               <span className="hidden sm:block">HERO.IO</span>
 //             </NavLink>
