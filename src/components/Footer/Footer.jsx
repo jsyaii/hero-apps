5b1f1eedeb217f5b5d51fd267613a8a7cda094e1
jsyaii <jasiahasan1703@gmail.com>
2025-12-25 12:31:40 +0600

    last commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -18,81 +18,81 @@ const Footer = () => {
               to="/"
               className="font-bold text-2xl text-white flex items-center gap-1"
             >
-//               <img className="w-[40px] h-[40px]" src={logo} alt="HERO.IO" />
-//               <span className="hidden sm:block">HERO.IO</span>
-//             </NavLink>
-//             <p className="text-sm mt-2">
-//               Discover & install amazing tools in one place.
-//             </p>
-//           </div>
+              <img className="w-[40px] h-[40px]" src={logo} alt="HERO.IO" />
+              <span className="hidden sm:block">HERO.IO</span>
+            </NavLink>
+            <p className="text-sm mt-2">
+              Discover & install amazing tools in one place.
+            </p>
+          </div>
 
-//           <div>
-//             <h3 className="text-xl font-medium text-white mb-2">Quick Links</h3>
-//             <ul className="space-y-1">
-//               {Links.map((link) => (
-//                 <li key={link.to} className="">
-//                   <NavLink
-//                     className={({ isActive }) =>
-//                       isActive ? "text-gradient " : " "
-//                     }
-//                     to={link.to}
-//                   >
-//                     {link.label}
-//                   </NavLink>
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
+          <div>
+            <h3 className="text-xl font-medium text-white mb-2">Quick Links</h3>
+            <ul className="space-y-1">
+              {Links.map((link) => (
+                <li key={link.to} className="">
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "text-gradient " : " "
+                    }
+                    to={link.to}
+                  >
+                    {link.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-//           <div>
-//             <h3 className="text-xl font-medium text-white mb-2">Support</h3>
-//             <ul className="space-y-1">
-//               <li>
-//                 <a href="#" className="hover:text-white">
-//                   Help Center
-//                 </a>
-//               </li>
-//               <li>
-//                 <a href="#" className="hover:text-white">
-//                   FAQs
-//                 </a>
-//               </li>
-//               <li>
-//                 <a href="#" className="hover:text-white">
-//                   Feedback
-//                 </a>
-//               </li>
-//             </ul>
-//           </div>
+          <div>
+            <h3 className="text-xl font-medium text-white mb-2">Support</h3>
+            <ul className="space-y-1">
+              <li>
+                <a href="#" className="hover:text-white">
+                  Help Center
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">
+                  FAQs
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-white">
+                  Feedback
+                </a>
+              </li>
+            </ul>
+          </div>
 
-//           <div>
-//             <h3 className="text-xl font-medium text-white mb-2">
-//               Social Links
-//             </h3>
-//             <div className="flex gap-4">
-//               <a
-//                 href="#"
-//                 className="w-[20px] h-[20px] bg-white text-black rounded-full flex items-center justify-center"
-//               >
-//                 <RiTwitterXFill />
-//               </a>
-//               <a
-//                 href="#"
-//                 className="w-[20px] h-[20px] bg-white text-black rounded-full flex items-center justify-center"
-//               >
-//                 <RiLinkedinFill />
-//               </a>
-//               <a href="#">
-//                 <MdFacebook className="text-[22px] text-white" />
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//         <hr className="my-6 border-gray-700" />
-//         <p className="text-center  ">Copyright © 2025 - All right reserved</p>
-//       </footer>
-//     </div>
-//   );
-// };
+          <div>
+            <h3 className="text-xl font-medium text-white mb-2">
+              Social Links
+            </h3>
+            <div className="flex gap-4">
+              <a
+                href="#"
+                className="w-[20px] h-[20px] bg-white text-black rounded-full flex items-center justify-center"
+              >
+                <RiTwitterXFill />
+              </a>
+              <a
+                href="#"
+                className="w-[20px] h-[20px] bg-white text-black rounded-full flex items-center justify-center"
+              >
+                <RiLinkedinFill />
+              </a>
+              <a href="#">
+                <MdFacebook className="text-[22px] text-white" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-700" />
+        <p className="text-center  ">Copyright © 2025 - All right reserved</p>
+      </footer>
+    </div>
+  );
+};
 
-// export default Footer;
+export default Footer;
